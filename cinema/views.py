9fcbc794cda4b599f118f3bdf2e751e8e9dc1e7a
MaglorFeanorson <daniilrog17@gmail.com>
2024-08-22 12:51:08 +0300

    "Fix: fixed issues"

--- cinema/views.py
+++ cinema/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.request import Request
 from rest_framework.response import Response
+
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
